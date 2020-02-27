2438ade11bd8783f244e5c76630560ea31ad1c1b
Guo-T460s <guoxiaozhong1990@outlook.com>
2020-02-28 02:57:50 +0800

    update to net462

--- samples/PowerShellSample/run.ps1
+++ samples/PowerShellSample/run.ps1
@@ -2,8 +2,8 @@
 [IO.Directory]::SetCurrentDirectory($PSScriptRoot)
 
 Add-Type -AssemblyName PresentationFramework
-[Reflection.Assembly]::LoadFrom('bin\Debug\net45\System.ValueTuple.dll') | Out-Null
-[Reflection.Assembly]::LoadFrom('bin\Debug\net45\ModernWpf.dll') | Out-Null
+[Reflection.Assembly]::LoadFrom('bin\Debug\net462\System.ValueTuple.dll') | Out-Null
+[Reflection.Assembly]::LoadFrom('bin\Debug\net462\ModernWpf.dll') | Out-Null
 
 $xaml = Get-Content "MainWindow.xaml" -Raw
 $window = [Windows.Markup.XamlReader]::Parse($xaml)
